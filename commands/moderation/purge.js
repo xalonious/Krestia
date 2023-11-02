af9c465f0682ed489f12f107c1905fc65c3dcf32
xalonious <xander.lauwens@gmail.com>
2023-11-02 20:16:57 +0100

    Added auto reply delete in purge command

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -1,31 +1,34 @@
 const { ApplicationCommandOptionType, PermissionsBitField } = require("discord.js")
 
 module.exports = {
-  name: "purge",
-  description: "clears chat messages",
-  permissionsRequired: [PermissionsBitField.Flags.ManageMessages],
-  options: [
-    {
-        name: "amount",
-        description: "the amount of messages to purge",
-        type: ApplicationCommandOptionType.Integer,
-        required: true
-    }
-  ],
+    name: "purge",
+    description: "clears chat messages",
+    permissionsRequired: [PermissionsBitField.Flags.ManageMessages],
+    options: [
+        {
+            name: "amount",
+            description: "the amount of messages to purge",
+            type: ApplicationCommandOptionType.Integer,
+            required: true
+        }
+    ],
 
-      run : async(client, interaction) => {
+    run: async (client, interaction) => {
+        const amount = interaction.options.getInteger("amount")
 
-      const amount = interaction.options.getInteger("amount")
+        if (parseInt(amount) > 99) {
+            return interaction.reply({
+                content: "I can only delete 99 messages at a time.",
+                ephemeral: true,
+            })
+        }
 
-         if(parseInt(amount) > 99) return interaction.reply({
-            content: "I can only delete 99 messages at a time.",
-            ephemeral: true
-         })
         await interaction.channel.bulkDelete(parseInt(amount))
-            .catch(err => console.log(err))
-            
-        interaction.reply({
-            content: `Succesfully cleared ${amount} messages`,
-        })
+
+        interaction.reply(`Successfully cleared ${amount} messages`)
+            setTimeout(() => {
+                interaction.deleteReply();
+            }, 3000);
+        
     }
 }
