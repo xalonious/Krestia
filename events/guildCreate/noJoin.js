acdf0d695e61d432f3383e80d6e65c5c58e100d6
xalonious <xander.lauwens@gmail.com>
2023-12-21 02:06:00 +0100

    Removed guildCreate event as it is unnecessary

--- events/guildCreate/noJoin.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client, guild) => {
-    if(guild.id !== "1074154728545583174") {
-        guild.owner.send("I automatically left server " + guild.name + " As I am a private bot that is specifically made for Krestia. Do not try to add me to other servers.")
-        guild.leave()
-        console.log("Left server " + guild.name + " | 401 Unauthorized")
-    }
-}
