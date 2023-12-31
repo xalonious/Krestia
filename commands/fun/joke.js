7f1df337d711291bb798a3b7478762a4c751d2da
Xalonious <xander.lauwens@gmail.com>
2023-12-31 16:06:09 +0100

    Bug fix in joke command

--- commands/fun/joke.js
+++ commands/fun/joke.js
@@ -12,11 +12,18 @@ module.exports = {
 
             const response = await axios.get('https://v2.jokeapi.dev/joke/Any');
             const joke = response.data;
+            
 
-        
+            let description = '';
+            if (joke.setup && joke.delivery) {
+                description = `${joke.setup}\n\n${joke.delivery}`;
+            } else if (joke.joke) {
+                description = joke.joke;
+            }
+
             const embed = new EmbedBuilder()
                 .setTitle(`Funny as f jokes by the best bot in town!! | Category: ${joke.category}`)
-                .setDescription(`${joke.setup}\n\n${joke.delivery}`)
+                .setDescription(description)
                 .setColor('#0099ff');
 
             await interaction.editReply({ embeds: [embed] });
