b7f828f06562037e910b9bc0ae68ef7b79b57079
Lauwens Xander <xander.lauwens@student.hogent.be>
2024-03-23 21:40:33 +0100

    Bug fix in getUserAvatar

--- utils/getUserAvatar.js
+++ utils/getUserAvatar.js
@@ -1,7 +1,8 @@
 const noblox = require("noblox.js")
 
 module.exports = async(userId) => {
-    const avatar = await noblox.getPlayerThumbnail(userId, 420, "png", false)
+    const avatarObject = await noblox.getPlayerThumbnail(userId, 420, "png", false)
+    const avatar = avatarObject[0].imageUrl
     if(avatar === "https://noblox.js.org/moderatedThumbnails/moderatedThumbnail_420x420.png") {
         return "https://img.favpng.com/0/16/15/woman-question-mark-female-png-favpng-JRuwedyFNNkcVvpqzN1dGbTEw.jpg"
     }
